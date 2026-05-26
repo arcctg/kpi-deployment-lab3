9c32ef77190776ad92d965f5015dfed82edc0ccc
arcctg <91179695+arcctg@users.noreply.github.com>
2026-05-26 20:35:25 +0300

    fix(deploy): run container as app user and normalize sudoers line endings

--- deploy/setup_target.sh
+++ deploy/setup_target.sh
@@ -71,7 +71,10 @@ chown root:app /etc/mywebapp/config.yaml
 chmod 640 /etc/mywebapp/config.yaml
 
 export IMAGE_REF
-envsubst < "${REPO_DEPLOY}/mywebapp.service.tmpl" > /etc/systemd/system/mywebapp.service
+APP_UID=$(id -u app)
+APP_GID=$(id -g app)
+export APP_UID APP_GID
+envsubst '${IMAGE_REF} ${APP_UID} ${APP_GID}' < "${REPO_DEPLOY}/mywebapp.service.tmpl" > /etc/systemd/system/mywebapp.service
 
 rm -f /etc/nginx/sites-enabled/default
 install -m 644 "${REPO_DEPLOY}/nginx.conf" /etc/nginx/sites-available/mywebapp
